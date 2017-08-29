aa7af7da2b17f545afaae20dc6f1435ef6f11d89
Eugene Kozlov <kozlovea8@gmail.com>
2017-08-29 18:25:07 +0300

    [add] facebook oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:facebook]
   
   validates :first_name, presence: :true, length: { maximum: 32 }
   validates :last_name,  presence: :true, length: { maximum: 64 }
+
+  def full_name
+    first_name + " " + last_name
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      
+      fullname = auth["info"]["name"].split(" ")
+      user.first_name = fullname.first
+      user.last_name = fullname.last
+    end
+  end
+  
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
