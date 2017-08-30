a0f60569a73e4ea93fcb2627704f7c123f83f378
Eugene Kozlov <kozlovea8@gmail.com>
2017-08-30 13:43:41 +0300

    [add] bootstrap to devise forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when :notice  then "alert alert-success"
+      when :success then "alert alert-info"
+      when :error   then "alert alert-danger"
+      when :alert   then "alert alert-warning"
+      else "alert alert-info"
+    end
+  end
 end
