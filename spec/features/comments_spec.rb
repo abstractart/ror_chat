494eb5e65e7368124a97ff8d82052b82759dfd5e
Eugene Kozlov <kozlovea8@gmail.com>
2017-08-30 15:40:07 +0300

    [add] specs for comments#index, comments#destroy

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -7,14 +7,39 @@ describe 'creating comment', type: :feature do
     login_as(user, scope: :user)
   end
 
-  it 'works well', js: true do
-    text = 'My Simple text'
-    visit '/'
-    click_link 'Add Comment'
-    within("#new_comment") do
-      fill_in 'Text', with: text
+  context 'index' do
+    before(:each) do
+      user.comments << Comment.new(text: FFaker::Lorem.characters(128))
     end
-    click_button 'Create Comment'
-    expect(page).to have_content text
+    it "shows all comments" do
+      visit '/'
+      expect(page).to have_css('.comment', count: 1)
+    end
+  end
+
+  context 'create comment' do
+    it 'works well', js: true do
+      text = 'My Simple text'
+      visit '/'
+      click_link 'Add Comment'
+      within("#new_comment") do
+        fill_in 'Text', with: text
+      end
+      click_button 'Create Comment'
+      expect(page).to have_content text
+    end
+  end
+
+  context 'delete comment' do
+    it "deletes well", js: true do
+      text = 'My Simple text'
+      user.comments << Comment.new(text: text)
+      visit '/'
+      click_link '×'
+      expect(page).to have_no_content(text)
+    end
+  end
+
+  context 'edit_comment' do
   end
 end
